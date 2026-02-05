44541952c6963f581721ea911305429d7fb67c27
Enzo D Prado <enzodp1501@gmail.com>
2026-02-05 09:24:25 -0300

    feat: setting spring data configs

--- src/main/java/study/lab/workspace/WorkspaceApplication.java
+++ src/main/java/study/lab/workspace/WorkspaceApplication.java
@@ -1,13 +1,27 @@
 package study.lab.workspace;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class WorkspaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkspaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceApplication.class, args);
 
+        log.info("""
+                             \s
+                
+                 _____________________________.___ _______    ________           __      __________ __________ ____  __.  ___________________  _____  _________ ___________
+                 /   _____/\\______   \\______   \\   |\\      \\  /  _____/          /  \\    /  \\_____  \\\\______   \\    |/ _| /   _____/\\______   \\/  _  \\ \\_   ___ \\\\_   _____/
+                 \\_____  \\  |     ___/|       _/   |/   |   \\/   \\  ___   ______ \\   \\/\\/   //   |   \\|       _/      <   \\_____  \\  |     ___/  /_\\  \\/    \\  \\/ |    __)_\s
+                 /        \\ |    |    |    |   \\   /    |    \\    \\_\\  \\ /_____/  \\        //    |    \\    |   \\    |  \\  /        \\ |    |  /    |    \\     \\____|        \\
+                /_______  / |____|    |____|_  /___\\____|__  /\\______  /           \\__/\\  / \\_______  /____|_  /____|__ \\/_______  / |____|  \\____|__  /\\______  /_______  /
+                        \\/                   \\/            \\/        \\/                 \\/          \\/       \\/        \\/        \\/                  \\/        \\/        \\/\s
+                             \s
+                              SPRING-WORKSPACE :: 0.1
+                            \s""");
+    }
 }
---
+++ src/main/java/study/lab/workspace/infrastructure/controller/UserController.java
@@ -0,0 +1,10 @@
+package study.lab.workspace.infrastructure.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
